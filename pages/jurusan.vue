c9c180b7ad64bcf8bb345a31148aba99ed795315
zaskianh <144655791+zaskianh@users.noreply.github.com>
2025-01-04 17:53:57 +0700

    Add files via upload

---
+++ pages/jurusan.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="py-12 bg">
+  <div class="container mx-auto px-4 text-center">
+      <h2 class="text-3xl font-bold text-gray-800 mb-6">Konsentrasi Keahlian SMK Negeri 4 Tasikmalaya</h2>
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+
+          <!-- TJKT -->
+          <div class="bg-white p-4 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+              <div class="flex justify-center mb-4 py-4">
+                  <img src="/assets/css/img/jurusan/tjkt.jpg">
+              </div>
+              <h3 class="text-lg font-semibold text-gray-800 mb-2">TJKT</h3>
+              <p class="text-gray-500 text-sm">Teknik Jaringan Komputer dan Telekomunikasi</p>
+          </div>
+
+          <!-- PPLG -->
+          <div class="bg-white p-4 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+              <div class="flex justify-center mb-4 py-4">
+                  <img src="/assets/css/img/jurusan/pplg.jpg">
+              </div>
+              <h3 class="text-lg font-semibold text-gray-800 mb-2">PPLG</h3>
+              <p class="text-gray-500 text-sm">Pengembangan Perangkat Lunak dan Gim</p>
+          </div>
+
+          <!-- TBSM -->
+          <div class="bg-white p-4 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+              <div class="flex justify-center mb-4 py-4">
+                  <img src="/assets/css/img/jurusan/tbsm.jpg">
+              </div>
+              <h3 class="text-lg font-semibold text-gray-800 mb-2">TBSM</h3>
+              <p class="text-gray-500 text-sm">Teknik Bisnis Sepeda Motor</p>
+          </div>
+
+          <!-- DKV -->
+          <div class="bg-white p-4 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+              <div class="flex justify-center mb-4 py-4">
+                <img src="/assets/css/img/jurusan/dkv.jpg">
+              </div>
+              <h3 class="text-lg font-semibold text-gray-800 mb-2">DKV</h3>
+              <p class="text-gray-500 text-sm">Desain Komunikasi Visual</p>
+          </div>
+
+          <!-- TOI -->
+          <div class="bg-white p-4 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+              <div class="flex justify-center mb-4 py-4">
+                  <img src="/assets/css/img/jurusan/toi.jpg">
+              </div>
+              <h3 class="text-lg font-semibold text-gray-800 mb-2">TOI</h3>
+              <p class="text-gray-500 text-sm">Teknik Otomasi Industri</p>
+          </div>
+          </div>
+          </div>
+          </section>
+</template>
+
+<style>
+.bg{
+  background-image: url('/assets/css/img/background/pattern.jpg');
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+}
+
+</style>
